4cb05414008c01a7a4cfa940b5cde258507c9b62
arantxa <149501497+arantxaMain@users.noreply.github.com>
2025-04-19 13:45:32 +0200

    arreglada barra backwards (más o menos)

--- frontend/src/components/ProgressBackwards.tsx
+++ frontend/src/components/ProgressBackwards.tsx
@@ -1,23 +1,19 @@
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
-
-import { cn } from "../lib/utils"
+import '../styles/components/Progress.css'
 
 const ProgressBackwards = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+>(({ value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={cn(
-      "h-2 w-full overflow-hidden rounded-full bg-primary/20",
-      className
-    )}
+    className="progress-root"
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-matcha transition-all"
-      style={{ transform: `translateX(-${value || 0}%)` }}
+      className="progress-indicator"
+      style={{ transform: `translateX(${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
 ))
