4bca27f413bf73e3b4da497ca5de17b0c72232fb
arantxa <149501497+arantxaMain@users.noreply.github.com>
2025-04-20 14:43:31 +0200

    fix anchos

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,20 +7,13 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <header className="header">
       <h1>Nook's Study Corner</h1>
+      <span className="material-symbols-rounded">
+        account_circle
+      </span>
     </header>
-      <App />
+    <App />
     <footer className="footer">
       <div className="footer-text">
-        <p>
-          This is a personal website for Nook's study corner.
-          <br />
-          All the content on this website is my own work.
-          <br />
-          If you want to use any of the content on this website, please contact me.
-          <br />
-          <br />
-          <a href="#">Github</a>
-        </p>
         <p>Alarm.wav by Tempouser -- https://freesound.org/s/162851/ -- License: Attribution NonCommercial 3.0</p>
       </div>
     </footer>
